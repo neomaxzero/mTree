cd44905fdfa1d522c28abe2a9f9b6820a7ea99a4
Maximiliano Céspedes <maxicespedes7@hotmail.com>
2017-01-25 22:03:20 -0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,55 @@
+class BST {
+  constructor() {
+    this.root = null;  
+  }
+  
+  addLeaf(value) {
+    this.addLeafP(value, this.root)
+  }
+  
+  addLeafP(value, node) {
+    if(this.root == null){
+      this.root = this.createLeaf(value);
+    } else if(value < node.value){
+      if(node.left != null) {
+        this.addLeafP(value, node.left)
+      } else {
+        node.left = this.createLeaf(value)
+      }
+    } else if(value > node.value) {
+      if(node.right != null) {
+        this.addLeafP(value, node.right)
+      } else  {
+        node.right = this.createLeaf(value);
+      }
+    } else {
+      console.log('VALOR IGUAL');
+    }
+  }
+  
+  createLeaf(value) {
+    return new node(value);
+  }
+  
+  printTransversal(node = this.root) {
+    if(this.root) {
+      if(node.left) {
+        this.printTransversal(node.left)
+      };
+      console.log(node.value);
+      if(node.right) {
+        this.printTransversal(node.right);
+      }
+    } else {
+      console.log('Empty tree');
+    }
+  }
+}
+
+class node {
+  constructor(val) {
+    this.value = val;
+    this.left = null;
+    this.right = null;
+  }
+}
